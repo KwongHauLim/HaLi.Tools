cdf0a4560bc5eb38cb527bdc52a5133c352437a5
HauLim <haulim.kwong@gmail.com>
2019-10-15 23:38:16 +0800

    IHashCalc have  string GetHash(string str) enough

--- HaLi.Tools.Hashcode/SHA1.cs
+++ HaLi.Tools.Hashcode/SHA1.cs
@@ -11,15 +11,20 @@ namespace HaLi.Tools.Hashcode
     {
         public Encoding Encoding { get; set; } = Encoding.UTF8;
         public bool UpperCase { get; set; } = true;
-        
-        public string GetHash(string str)
+
+        public string GetHash(byte[] binary)
         {
             using (var sha1 = new SHA1Managed())
             {
-                var hash = sha1.ComputeHash(Encoding.GetBytes(str));
+                var hash = sha1.ComputeHash(binary);
                 var format = UpperCase ? "X2" : "x2";
                 return string.Concat(hash.Select(b => b.ToString(format)));
             }
+        }
+
+        public string GetHash(string str)
+        {
+            return GetHash(Encoding.GetBytes(str));
         }
     }
 }
