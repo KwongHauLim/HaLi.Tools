2b3232d2e519101564653e99300465d0a2449e1d
Kwong Hau Lim <haulim.kwong@gmail.com>
2019-10-15 18:34:07 +0800

    Sha1 hash

---
+++ HaLi.Tools.Hashcode/Hash.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HaLi.Tools.Hashcode
+{
+    public interface IHashCalc
+    {
+        string GetHash(byte[] binary);
+    }
+
+    public class Hash
+    {
+    }
+}
---
+++ HaLi.Tools.Hashcode/SHA1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HaLi.Tools.Hashcode
+{
+    public sealed class SHA1 : IHashCalc
+    {
+        public string GetHash(byte[] binary)
+        {
+            using (var sha1 = new SHA1Managed())
+            {
+                var hash = sha1.ComputeHash(binary);
+                return string.Concat(hash.Select(b => b.ToString("X2")));
+            }
+        }
+    }
+}
