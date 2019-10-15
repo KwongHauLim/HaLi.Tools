329d2528f8db48729f1cf0d77ba95ffff8209868
Kwong Hau Lim <haulim.kwong@gmail.com>
2019-10-15 18:11:18 +0800

    Base64

---
+++ HaLi.Tools.Encryption/HaLi.Tools.Encryption/Base64.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace HaLi.Tools.Encryption
+{
+    public class Base64 : ICrypto
+    {
+        public Encoding Encoding { get; set; } = Encoding.UTF8;
+        public bool UpperCase { get; set; } = true;
+
+        public string Decrypt(string str)
+        {
+            byte[] bin = Encoding.GetBytes(str);
+            string encode = Convert.ToBase64String(bin);
+            return UpperCase ? encode.ToUpper() : encode.ToLower();
+        }
+
+        public string Encrypt(string str)
+        {
+            byte[] bin = Convert.FromBase64String(str);
+            return Encoding.GetString(bin);
+        }
+    }
+}
--- HaLi.Tools.Encryption/HaLi.Tools.Encryption/Crypto.cs
+++ HaLi.Tools.Encryption/HaLi.Tools.Encryption/Crypto.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HaLi.Tools.Encryption
+﻿namespace HaLi.Tools.Encryption
 {
     public interface ICrypto
     {
