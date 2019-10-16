e2e5ae910d1b46140563d8d4d677f08f55f32d1b
HauLim <haulim.kwong@gmail.com>
2019-10-17 00:38:26 +0800

    DebuggerStepThrough

--- HaLi.Tools.Extends/ExtendList.cs
+++ HaLi.Tools.Extends/ExtendList.cs
@@ -1,11 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace HaLi.Tools.Extends
 {
+    [DebuggerStepThrough]
     public static class ExtendList
     {
         public static IEnumerable<int> Range(int count, int start = 0)
