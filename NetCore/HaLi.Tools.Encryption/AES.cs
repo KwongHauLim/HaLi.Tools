e67b8ebf439528265b8871b7353304f160f38e81
Kwong Hau Lim <haulim.kwong@gmail.com>
2020-10-17 18:32:08 +0800

    Cryption

--- NetCore/HaLi.Tools.Encryption/AES.cs
+++ NetCore/HaLi.Tools.Encryption/AES.cs
@@ -22,6 +22,25 @@ namespace HaLi.Tools.Encryption
 
         public CryptoBook Secret { get; set; }
 
+        public static AES @Default => new AES
+        {
+            Secret = new AES.CryptoBook("Te6BZKhwp5HfYA6OQsinNlkM1CN97FQtE4NdWe9WwBM=", "L0g9aJpY6hvQ2xAddS6GIA==")
+        };
+
+        public static void GenerateKey(out string key, out string iv)
+        {
+            using AesManaged aes = new AesManaged();
+            key = Convert.ToBase64String(aes.Key);
+            iv = Convert.ToBase64String(aes.IV);
+        }
+
+        public static void GenerateKey(out byte[] key, out byte[] iv)
+        {
+            using AesManaged aes = new AesManaged();
+            key = aes.Key;
+            iv = aes.IV;
+        }
+
         public string Encrypt(string str)
         {
             using (AesManaged aes = new AesManaged() { Key = Secret.KEY, IV = Secret.IV })
