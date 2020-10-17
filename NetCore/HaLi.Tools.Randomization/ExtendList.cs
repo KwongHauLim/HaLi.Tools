261c951519be2c75ff37434ac8fce8e79be71b24
Kwong Hau Lim <haulim.kwong@gmail.com>
2020-10-17 17:38:30 +0800

    Extend shuffle

--- NetCore/HaLi.Tools.Randomization/ExtendList.cs
+++ NetCore/HaLi.Tools.Randomization/ExtendList.cs
@@ -9,29 +9,55 @@ namespace HaLi.Tools.Randomization
     [DebuggerStepThrough]
     public static class ExtendList
     {
-        public static async void Shuffle<T>(this List<T> list)
-            => await ShuffleAsync(list);
+        /// <summary>
+        /// Shuffle the array
+        /// </summary>
+        public static void Shuffle<T>(this T[] array)
+            => ShuffleAsync(array).Wait();
 
+        /// <summary>
+        /// Shuffle the array
+        /// </summary>
+        public static Task ShuffleAsync<T>(this T[] array)
+            => Shuffle(array.Length, (a, b) =>
+            {
+                T tmp = array[a];
+                array[a] = array[b];
+                array[b] = tmp;
+            });
+
+        /// <summary>
+        /// Shuffle the List
+        /// </summary>
+        public static void Shuffle<T>(this List<T> list)
+            => ShuffleAsync(list).Wait();
+
+        /// <summary>
+        /// Shuffle the List
+        /// </summary>
         public static Task ShuffleAsync<T>(this List<T> list)
+            => Shuffle(list.Count, (a, b) =>
+            {
+                T tmp = list[a];
+                list[a] = list[b];
+                list[b] = tmp;
+            });
+
+        private static Task Shuffle(int count, Action<int,int> indicate)
             => RNG.Share.Pool.DoAsync((rand) =>
             {
                 // Fisher-Yates shuffle
                 // http://en.wikipedia.org/wiki/Fisher-Yates_shuffle
-                T tmp;
-                int cnt = list.Count;
                 int idx;
-                for (int i = cnt - 1; i > 0; i--)
+                for (int i = count - 1; i > 0; i--)
                 {
-                    idx = Math.Abs(rand.Int32) % cnt;
+                    idx = Math.Abs(rand.Int32) % count;
                     if (idx != i)
                     {
-                        tmp = list[idx];
-                        list[idx] = list[i];
-                        list[i] = tmp;
+                        indicate(idx, i);
                     }
                     Thread.Sleep(0);
                 }
             });
-
     }
 }
