eda4dc6a5739f67c924379fa97d4ff567a003e8a
Kwong Hau Lim <haulim.kwong@outlook.com>
2024-12-18 23:23:12 +0800

    ExtendSpan

---
+++ Net6+/HaLi.Tools/Extends/ExtendSpan.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HaLi.Tools.Extends;
+
+[DebuggerStepThrough]
+public static class ExtendSpan
+{
+    public static string ToHexString(this ReadOnlySpan<byte> bytes)
+    {
+        var sb = new StringBuilder(bytes.Length * 2);
+        for (var i = 0; i < bytes.Length; i++)
+        {
+            sb.Append(bytes[i].ToString("X2"));
+        }
+        return sb.ToString();
+    }
+}
